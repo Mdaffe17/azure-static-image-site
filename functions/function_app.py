583a26717e1a6f757d13922606f7ccac1779c544
Mdaffe17 <mouctardaffe99@gmail.com>
2025-12-19 20:42:37 -0500

    backend: redimensionnement automatique des images via Blob Trigger

--- functions/function_app.py
+++ functions/function_app.py
@@ -1,6 +1,8 @@
 import logging
 import azure.functions as func
 from azure.storage.blob import BlobServiceClient
+from PIL import Image
+import io
 import os
 import uuid
 
@@ -53,3 +55,46 @@ def upload_image(req: func.HttpRequest) -> func.HttpResponse:
             f"Erreur serveur : {str(e)}",
             status_code=500
         )
+
+
+
+
+@app.blob_trigger(arg_name="myblob", path="images-original/{name}",
+                               connection="AzureWebJobsStorage") 
+def resize_image(myblob: func.InputStream):
+    logging.info(f"Redimensionnement de l'image : {myblob.name}")
+
+    image_bytes = myblob.read()
+    image = Image.open(io.BytesIO(image_bytes))
+    image = image.convert("RGB")
+
+    image.thumbnail((256, 256))
+
+    output = io.BytesIO()
+    image.save(output, format="JPEG")
+    output.seek(0)
+
+    connection_string = os.getenv("AzureWebJobsStorage")
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+    filename = os.path.basename(myblob.name)
+
+    container_client = blob_service_client.get_container_client("images-thumbnails")
+    blob_client = container_client.get_blob_client(filename)
+    blob_client.upload_blob(output, overwrite=True)
+
+    logging.info(f"Miniature créée : {filename}")
+
+# This example uses SDK types to directly access the underlying BlobClient object provided by the Blob storage trigger.
+# To use, uncomment the section below and add azurefunctions-extensions-bindings-blob to your requirements.txt file
+# Ref: aka.ms/functions-sdk-blob-python
+#
+# import azurefunctions.extensions.bindings.blob as blob
+# @app.blob_trigger(arg_name="client", path="images-original/{name}",
+#                   connection="AzureWebJobsStorage")
+# def resize_image(client: blob.BlobClient):
+#     logging.info(
+#         f"Python blob trigger function processed blob \n"
+#         f"Properties: {client.get_blob_properties()}\n"
+#         f"Blob content head: {client.download_blob().read(size=1)}"
+#     )
